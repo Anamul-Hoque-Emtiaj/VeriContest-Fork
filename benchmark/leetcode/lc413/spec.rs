f4a1a09a308add9b802c70f5920aa1e25c7ab1f5
Grayson Xie <zichenxie0106@gmail.com>
2026-07-10 10:23:03 -0400

    fix lc413

--- benchmark/leetcode/lc413/spec.rs
+++ benchmark/leetcode/lc413/spec.rs
@@ -1,50 +1,52 @@
 use vstd::prelude::*;
 
+fn main() {}
+
 verus! {
 
+pub struct Solution;
 
-
-
-pub open spec fn spec_run(nums: Seq<i32>, i: int) -> int
-    decreases i,
-{
-    if i < 2 {
-        0
-    } else if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2] {
-        spec_run(nums, i - 1) + 1
-    } else {
-        0
+impl Solution {
+    pub open spec fn spec_run(nums: Seq<i32>, i: int) -> int
+        decreases i,
+    {
+        if i < 2 {
+            0
+        } else if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2] {
+            Self::spec_run(nums, i - 1) + 1
+        } else {
+            0
+        }
     }
-}
 
-
-pub open spec fn spec_total(nums: Seq<i32>, k: int) -> int
-    decreases k,
-{
-    if k < 2 {
-        0
-    } else {
-        spec_total(nums, k - 1) + spec_run(nums, k)
+    pub open spec fn spec_total(nums: Seq<i32>, k: int) -> int
+        decreases k,
+    {
+        if k < 2 {
+            0
+        } else {
+            Self::spec_total(nums, k - 1) + Self::spec_run(nums, k)
+        }
     }
-}
 
-pub open spec fn spec_number_of_arithmetic_slices(nums: Seq<i32>) -> int
-{
-    if nums.len() < 3 {
-        0
-    } else {
-        spec_total(nums, (nums.len() - 1) as int)
+    pub open spec fn spec_number_of_arithmetic_slices(nums: Seq<i32>) -> int
+    {
+        if nums.len() < 3 {
+            0
+        } else {
+            Self::spec_total(nums, (nums.len() - 1) as int)
+        }
     }
-}
 
-fn number_of_arithmetic_slices(nums: Vec<i32>) -> (result: i32)
-    requires
-        nums.len() >= 1,
-        nums.len() <= 5000,
-        forall|i: int| 0 <= i < nums.len() ==> -1000 <= #[trigger] nums[i] <= 1000,
-    ensures
-        result as int == spec_number_of_arithmetic_slices(nums@),
-{
+    fn number_of_arithmetic_slices(nums: Vec<i32>) -> (result: i32)
+        requires
+            nums.len() >= 1,
+            nums.len() <= 5000,
+            forall|i: int| 0 <= i < nums.len() ==> -1000 <= #[trigger] nums[i] <= 1000,
+        ensures
+            result as int == Self::spec_number_of_arithmetic_slices(nums@),
+    {
+    }
 }
 
 }
